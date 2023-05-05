6ba41c1d28dc7611ee5a0e3b193c015219637471
MIDnightZ <hongfoo1204@gmail.com>
2023-05-05 18:42:05 +0800

    disable zoom for spline, added header for tech stack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Heading, VStack } from '@chakra-ui/react'
+import { Box, HStack, Heading, Text, VStack } from '@chakra-ui/react'
 import Spline from '@splinetool/react-spline'
 import { Inter } from 'next/font/google'
 
@@ -6,24 +6,30 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <HStack>
-      <VStack w="50%" alignItems="flex-start">
-        <Heading>Hi 👋,</Heading>
-        <Heading display="flex" flexDirection="row">
-          I am{' '}
-          <Heading
-            ml="2.5"
-            bgGradient="linear(to-r, #13B0F5, #E70FAA)"
-            bgClip="text"
-          >
-            Hong Zee
+    <>
+      <HStack>
+        <VStack w="50%" alignItems="flex-start">
+          <Heading>Hi 👋,</Heading>
+          <Heading display="flex" flexDirection="row">
+            I am{' '}
+            <Text
+              ml="2.5"
+              bgGradient="linear(to-r, #13B0F5, #E70FAA)"
+              bgClip="text"
+            >
+              Hong Zee
+            </Text>
           </Heading>
-        </Heading>
-        <Heading>I build things for web</Heading>
-      </VStack>
-      <Box w="50%" height="800px">
-        <Spline scene="https://prod.spline.design/6ZeBl64Z6UpMXEOx/scene.splinecode" />
-      </Box>
-    </HStack>
+          <Heading>I build things for web</Heading>
+        </VStack>
+        <Box w="50%" height="800px">
+          <Spline scene="https://prod.spline.design/CW6VddQuRlUr8OnH/scene.splinecode" />
+        </Box>
+      </HStack>
+      <Heading textAlign="center">My Tech Stack</Heading>
+      <Text textAlign="center">
+        Technologies I’ve been working with recently
+      </Text>
+    </>
   )
 }
